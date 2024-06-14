2620b0a2658a4aaf4b612a9036fa1a93daed0cce
Arif-Samiaji11 <138750132+Arif-Samiaji11@users.noreply.github.com>
2024-06-14 14:56:39 +0700

    code saya

---
+++ .history/app_20240613005513.py
@@ -0,0 +1,142 @@
+import os
+from ultralytics import YOLO
+from ultralytics.solutions import object_counter
+import cv2
+import pandas as pd
+from pymongo import MongoClient
+from datetime import datetime
+
+# Fungsi untuk mendeteksi kendaraan IN berdasarkan lintasan
+
+
+def is_vehicle_in(current_box, previous_box):
+    # Asumsi bahwa arah gerakan dari bawah ke atas dianggap sebagai IN
+    if previous_box and current_box[1] < previous_box[1]:
+        return True
+    return False
+
+# Fungsi untuk menyimpan hasil deteksi ke MongoDB
+
+
+def save_to_mongodb(detections, collection):
+    if detections:
+        try:
+            collection.insert_many(detections)
+            print(f"Inserted {len(detections)} records into MongoDB.")
+        except Exception as e:
+            print(f"Error saving to MongoDB: {e}")
+
+# Fungsi untuk mengambil data dari MongoDB dan menyimpannya ke CSV
+
+
+def export_to_csv(collection):
+    try:
+        data = list(collection.find())
+        if not data:
+            print("No data found in MongoDB collection.")
+            return
+
+        df = pd.DataFrame(data)
+        required_columns = {'jenis_kendaraan', 'date', 'deteksi', 'hari'}
+        if not required_columns.issubset(df.columns):
+            print(
+                f"Required columns are missing from data: {required_columns - set(df.columns)}")
+            return
+
+    except Exception as e:
+        print(f"Error reading from MongoDB: {e}")
+        return
+
+    # Hitung jumlah deteksi per jenis kendaraan per hari
+    deteksi_per_hari = df.groupby(['date', 'jenis_kendaraan']).agg({
+        'deteksi': 'sum'
+    }).reset_index()
+
+    # Simpan hasil ke CSV
+    csv_file = 'hasil_deteksi_kendaraan.csv'
+    deteksi_per_hari.to_csv(csv_file, index=False)
+    print(f"Data has been exported to {csv_file}")
+
+
+# Inisialisasi koneksi MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['db_data']
+collection = db['hasil_deteksi']
+
+# Inisialisasi model YOLO
+try:
+    model = YOLO('best.pt')
+except Exception as e:
+    print(f"Error loading model: {e}")
+    exit()
+
+# Path video yang akan digunakan (gunakan jalur absolut)
+video_path = os.path.abspath('video2 (1).mp4')
+
+# Check if video file exists
+if not os.path.exists(video_path):
+    print(f"Video file '{video_path}' not found.")
+    exit()
+
+# Buka file video
+cap = cv2.VideoCapture(video_path)
+assert cap.isOpened(), "Error opening video file"
+
+# Dapatkan informasi video
+w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH,
+             cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+
+region_of_interest = [(20, 600), (1700, 604), (1700, 560), (20, 560)]
+
+# Video writer
+video_writer = cv2.VideoWriter(
+    "object_counting_output.avi", cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+
+# Init Object Counter
+counter = object_counter.ObjectCounter()
+counter.set_args(view_img=True, reg_pts=region_of_interest,
+                 classes_names=model.names, draw_tracks=True)
+
+frame_count = 0
+previous_boxes = {}
+detections = []
+
+# Proses setiap frame
+while cap.isOpened():
+    success, im0 = cap.read()
+    if not success:
+        print("Video frame is empty or video processing has been successfully completed.")
+        break
+    tracks = model.track(im0, persist=True, show=False)
+    im0 = counter.start_counting(im0, tracks)
+    video_writer.write(im0)
+    frame_count += 1
+
+    # Kumpulkan data deteksi
+    for prediction in tracks:
+        for i, box in enumerate(prediction.boxes):
+            current_box = box.xyxy[0].tolist()
+            if is_vehicle_in(current_box, previous_boxes.get(i)):
+                class_index = int(box.cls[0])
+                class_name = model.names[class_index]
+                timestamp = datetime.now()
+                document = {
+                    "frame_number": frame_count,
+                    "jenis_kendaraan": class_name,
+                    "deteksi": 1,  # Setiap entri dihitung sebagai satu deteksi
+                    "date": timestamp.strftime('%Y-%m-%d'),
+                    "hari": timestamp.strftime('%A'),
+                    "timestamp": timestamp.strftime('%Y-%m-%d %H:%M:%S')
+                }
+                detections.append(document)
+            previous_boxes[i] = current_box
+
+cap.release()
+video_writer.release()
+cv2.destroyAllWindows()
+
+# Simpan semua data deteksi ke MongoDB
+save_to_mongodb(detections, collection)
+
+# Ekspor data ke CSV
+export_to_csv(collection)
---
+++ .history/strimlit_20240613233927.py
@@ -0,0 +1,153 @@
+import os
+from pymongo import MongoClient
+import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
+
+# Fungsi untuk mengubah halaman menjadi tampilan yang lebih menarik
+
+
+def set_page_layout():
+    st.markdown(
+        """
+        <style>
+        .reportview-container {
+            background: linear-gradient(135deg, #f3ec78 0%, #af4261 100%);
+            color: #333333;
+        }
+        .sidebar .sidebar-content {
+            background: linear-gradient(135deg, #232526 0%, #414345 100%);
+            color: #ffffff;
+        }
+        .sidebar .sidebar-content .stButton>button {
+            background-color: #64dfdf;
+            color: #ffffff;
+            border: none;
+            padding: 8px 16px;
+            border-radius: 5px;
+            transition: background-color 0.3s;
+        }
+        .sidebar .sidebar-content .stButton>button:hover {
+            background-color: #5bb6b6;
+            cursor: pointer;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+# Fungsi untuk menggambar diagram dan menampilkan tabel
+
+
+def draw_charts(df):
+    car = df[df['jenis_kendaraan'] == 'car'].shape[0]
+    truck = df[df['jenis_kendaraan'] == 'truck'].shape[0]
+    total = car + truck
+
+    labels = ['Car', 'Truck']
+    sizes = [car, truck]
+    persentase_cars = car / total * 100 if total > 0 else 0
+    persentase_trucks = truck / total * 100 if total > 0 else 0
+
+    data = {
+        'Kategori': ['Car', 'Truck'],
+        'Jumlah': [car, truck],
+        'Persentase': [f'{persentase_cars:.2f}%', f'{persentase_trucks:.2f}%']
+    }
+
+    if os.path.isfile('history.csv'):
+        history_df = pd.read_csv('history.csv')
+        history_df = pd.concat(
+            [history_df, pd.DataFrame([data])], ignore_index=True)
+    else:
+        history_df = pd.DataFrame([data])
+    history_df.to_csv('history.csv', index=False)
+
+    fig, ax = plt.subplots()
+    explode = (0, 0.1)
+    ax.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+           startangle=90, colors=['#ff9999', '#66b3ff'])
+    ax.axis('equal')
+    st.sidebar.pyplot(fig)
+
+    fig, ax = plt.subplots()
+    ax.bar(labels, sizes, color=['#ff9999', '#66b3ff'])
+    ax.set_ylabel('Jumlah')
+    ax.set_title('Jumlah Kendaraan (Bar Chart)')
+    st.sidebar.pyplot(fig)
+
+    fig, ax = plt.subplots()
+    ax.fill_between(labels, sizes, color='skyblue', alpha=0.4)
+    ax.plot(labels, sizes, marker='o', color='Slateblue', alpha=0.6)
+    ax.set_ylabel('Jumlah')
+    ax.set_title('Jumlah Kendaraan (Area Chart)')
+    st.sidebar.pyplot(fig)
+
+    fig, ax = plt.subplots()
+    ax.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+           startangle=90, wedgeprops=dict(width=0.3), colors=['#ff9999', '#66b3ff'])
+    ax.axis('equal')
+    st.sidebar.pyplot(fig)
+
+    table = pd.DataFrame(data)
+
+    st.write("Total Objek Terdeteksi:")
+    st.table(table)
+
+# Fungsi untuk menampilkan data history
+
+
+def show_history(history_df):
+    if history_df.empty:
+        st.write("Belum ada data history yang tersimpan.")
+    else:
+        st.write("### History")
+        for index, row in history_df.iterrows():
+            if st.button(f"Hapus Baris {index}"):
+                history_df.drop(index, inplace=True)
+                history_df.to_csv('history.csv', index=False)
+                st.success(f"Baris {index} berhasil dihapus dari history.")
+            st.write(row)
+
+# Fungsi untuk mengambil data dari MongoDB
+
+
+def load_data_from_mongodb():
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["db_data"]
+    collection = db["hasil_deteksi"]
+    data = list(collection.find())
+    for document in data:
+        document['_id'] = str(document['_id'])
+    df = pd.DataFrame(data)
+    return df
+
+
+# Set tata letak halaman dan warna latar belakang
+set_page_layout()
+
+option = st.sidebar.selectbox(
+    'Silakan pilih:',
+    ('Home', 'Dataframe', 'History')
+)
+
+if option == 'Home' or option == '':
+    st.title("Selamat Datang di Halaman Utama")
+    st.write("Di sini Anda dapat memilih untuk melihat data atau visualisasi.")
+elif option == 'Dataframe':
+    st.title("Dataframe")
+    try:
+        df = load_data_from_mongodb()
+        st.dataframe(df)
+        draw_charts(df)
+        if st.button('Simpan Data Visualisasi ke History'):
+            st.write("Data visualisasi berhasil disimpan ke history.")
+    except Exception as e:
+        st.error(f"Gagal mengambil data dari MongoDB: {e}")
+elif option == 'History':
+    st.title("History")
+    try:
+        history_df = pd.read_csv('history.csv')
+    except FileNotFoundError:
+        history_df = pd.DataFrame(columns=['Kategori', 'Jumlah', 'Persentase'])
+    show_history(history_df)
